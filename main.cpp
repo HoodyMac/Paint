2731ea02ff12a1178a85dbe2439b867a781d0dd7
Maksym Osmuk <maxim.osmuk@gmail.com>
2017-04-08 22:15:04 +0200

    Add color selection

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <windows.h>
 
 #define SCREEN_WIDTH 80
-#define SCREEN_HEIGHT 50
+#define SCREEN_HEIGHT 52
+#define CANVAS_HEIGHT 50
+
+int a = 1;
+int currentColor = BACKGROUND_GREEN;
+
+void draw(int x, int y, CHAR_INFO screenBuffer[], int color) {
+	screenBuffer[x + y * SCREEN_WIDTH].Attributes = color;
+}
 
 bool input(CHAR_INFO screenBuffer[]) {
 	INPUT_RECORD inputRecord;
@@ -12,16 +20,22 @@ bool input(CHAR_INFO screenBuffer[]) {
 	ReadConsoleInput(hInput, &inputRecord, 1, &events);
 
 	if (inputRecord.EventType == MOUSE_EVENT) {
+		int x = inputRecord.Event.MouseEvent.dwMousePosition.X;
+		int y = inputRecord.Event.MouseEvent.dwMousePosition.Y;
 		if (inputRecord.Event.MouseEvent.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED) {
-			int x = inputRecord.Event.MouseEvent.dwMousePosition.X;
-			int y = inputRecord.Event.MouseEvent.dwMousePosition.Y;
-			screenBuffer[x + y * SCREEN_WIDTH].Attributes = BACKGROUND_GREEN;
+			if (y > CANVAS_HEIGHT && x < 16) {
+				screenBuffer[(currentColor >> 4) + (y - 1) * SCREEN_WIDTH].Char.AsciiChar = ' ';
+				currentColor = inputRecord.Event.MouseEvent.dwMousePosition.X << 4;
+				screenBuffer[x + (y - 1) * SCREEN_WIDTH].Char.AsciiChar = 'V';
+				screenBuffer[x + (y - 1) * SCREEN_WIDTH].Attributes = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED;
+			}
+			else {
+				draw(x, y, screenBuffer, currentColor);
+			}
 			return true;
 		}
 		else if (inputRecord.Event.MouseEvent.dwButtonState == RIGHTMOST_BUTTON_PRESSED) {
-			int x = inputRecord.Event.MouseEvent.dwMousePosition.X;
-			int y = inputRecord.Event.MouseEvent.dwMousePosition.Y;
-			screenBuffer[x + y * SCREEN_WIDTH].Attributes = 0;
+			draw(x, y, screenBuffer, 0);
 			return true;
 		}
 	}
@@ -45,13 +59,17 @@ int main() {
 
 	for (int y = 0; y < SCREEN_HEIGHT; y++) {
 		for (int x = 0; x < SCREEN_WIDTH; x++) {
+			if (y > CANVAS_HEIGHT && x < 16) {
+				screenBuffer[x + y * SCREEN_WIDTH].Attributes = (x % 16) << 4;
+			}
 			screenBuffer[x + y * SCREEN_WIDTH].Char.AsciiChar = ' ';
-			//screenBuffer[x + y * SCREEN_WIDTH].Attributes = BACKGROUND_GREEN;
 		}
 	}
 
 	SMALL_RECT canvasRect = { 0, 0, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1 };
 	COORD writeStart = { 0, 0 };
+
+	WriteConsoleOutput(hOutput, screenBuffer, bufferSize, writeStart, &canvasRect);
 
 	while (1) {
 		if (input(screenBuffer)) {
